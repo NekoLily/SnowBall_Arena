ec208c4b58d4ee578010c9ffd4d7f9f3686d5ede
Jessy <jessy-lin@hotmail.fr>
2019-06-12 17:06:56 +0200

    Menu

---
+++ Snowball_Arena/Assets/Menu/Volume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Volume : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    
+    public AudioClip backsound;
+    private AudioSource source;
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+}
