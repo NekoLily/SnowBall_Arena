053156d696a5088bfdf8865c7d741b7da08c67ea
DESKTOP-VEHLP86\squas <lq.millac@hotmail.com>
2019-06-12 17:19:17 +0200

    scrip scale a modif ^^

---
+++ Snowball_Arena/Assets/Scale.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour
+{
+    int limitemax = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Snow")
+        {
+            if (limitemax <= 200)
+            {
+                Debug.Log("devient gros");
+                transform.localScale += new Vector3(0.12F, 0.12F, 0);
+                limitemax += 1;
+            }
+            if (limitemax <= 400)
+            {
+                Debug.Log("devient gros");
+                transform.localScale += new Vector3(0.10F, 0.10F, 0);
+                limitemax += 1;
+            }
+
+        }
+    }
+}
