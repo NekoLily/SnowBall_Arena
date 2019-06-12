a6e898c31704ef950ff7289173a16ff45afba3ff
François Charrier <francois.charrier23@gmail.com>
2019-06-12 15:20:12 +0200

    Changement du scale de snowPrefab

--- Snowball_Arena/Assets/GameManager.cs
+++ Snowball_Arena/Assets/GameManager.cs
@@ -17,6 +17,12 @@ public class GameManager : MonoBehaviour
 		}
     }
     private void Awake(){
+        
+    }
+    /// <summary>
+    /// Instantiate the snow prefab based on the map size
+    /// </summary>
+    private void AddSnowToMap(){
         for(int currentHeightNum = (int)-(mapHeight/spriteHeight)-1; currentHeightNum< (mapHeight/spriteHeight); currentHeightNum++ ){
             for(int currentWidthNum = -(int)(mapWidth/spriteWidth)/2; currentWidthNum< (mapWidth/spriteWidth)/2; currentWidthNum++){  
                 Instantiate(snowPrefab,new Vector2((currentWidthNum * spriteWidth)+(spriteWidth/2), (currentHeightNum * spriteHeight)+(spriteWidth/2)), Quaternion.identity,mapParent);
