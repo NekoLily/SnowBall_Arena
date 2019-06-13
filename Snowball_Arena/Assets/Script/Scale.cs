62a6cd166c3c64add763d8aa924d47df7ebb7224
DESKTOP-VEHLP86\squas <lq.millac@hotmail.com>
2019-06-13 13:30:37 +0200

    shoot

--- Snowball_Arena/Assets/Script/Scale.cs
+++ Snowball_Arena/Assets/Script/Scale.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Scale : MonoBehaviour
 {
-    int currentScale = 0;
+    public int currentScale = 0;
     public void IncreaseScale()
     {
         if (currentScale <= 200)
