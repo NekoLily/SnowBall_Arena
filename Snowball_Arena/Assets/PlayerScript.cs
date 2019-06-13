7a81ed0ad7ccddea56876f7dc84ccf083bca5610
Neko <bernard.sourichanh@gmail.com>
2019-06-13 10:43:45 +0200

    Add : Firemode

--- Snowball_Arena/Assets/PlayerScript.cs
+++ Snowball_Arena/Assets/PlayerScript.cs
@@ -10,7 +10,9 @@ public enum PlayerState
 public class PlayerScript : MonoBehaviour
 {
     protected Rigidbody2D _Rigidbody2D;
-    public GameObject SnowBallShootPrefab;
+    public GameObject SnowBallMultiShootPrefab;
+    public GameObject SnowBallBigShootPrefab;
+    public GameObject SnowBallPiercingShootPrefab;
     public PlayerState _PlayerState = PlayerState.Moving;
 
     float ShootForce;
@@ -24,6 +26,7 @@ public class PlayerScript : MonoBehaviour
     public float MaxChargeForce = 100;
 
     private float Trigger = 0;
+    private int FireMode = 0;
 
     private void Start()
     {
@@ -43,8 +46,12 @@ public class PlayerScript : MonoBehaviour
         Vector2 MoveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         Vector2 TargetDirection = new Vector2(Input.GetAxis("HorizontalTarget"), Input.GetAxis("VerticalTarget"));
 
+        float LeftBumper = Input.GetAxis("LeftBumper");
+        float RightBumper = Input.GetAxis("RightBumper");
+
         Trigger = Input.GetAxis("Trigger");
 
+        ChangeFireMode(LeftBumper, RightBumper);
         MoveSnowBall(MoveDirection);
         Shoot(TargetDirection);
     }
@@ -78,11 +85,50 @@ public class PlayerScript : MonoBehaviour
             _PlayerState = PlayerState.LoadingShoot;
         }
         else if (Trigger == 0 && _PlayerState == PlayerState.LoadingShoot)
-        {            
-            GameObject SnowBall = Instantiate<GameObject>(SnowBallShootPrefab, transform.position, transform.rotation);
-            SnowBall.GetComponent<Rigidbody2D>().AddForce(TargetDirection * ShootForce);
+        {
+            GameObject SnowBall;
+            switch (FireMode)
+            {
+                case 0:
+                    SnowBall = Instantiate<GameObject>(SnowBallBigShootPrefab, transform.position, transform.rotation);
+                    SnowBall.GetComponent<Rigidbody2D>().AddForce(TargetDirection * ShootForce);
+                    break;
+
+                case 1:
+                    SnowBall = Instantiate<GameObject>(SnowBallMultiShootPrefab, transform.position, transform.rotation);
+                    break;
+
+                case 2:
+                    SnowBall = Instantiate<GameObject>(SnowBallPiercingShootPrefab, transform.position, transform.rotation);
+                    SnowBall.GetComponent<Rigidbody2D>().AddForce(TargetDirection * ShootForce);
+                    break;
+
+
+            }
+            
+            
             ShootForce = DefaultShootForce;
             _PlayerState = PlayerState.Moving;
+        }
+    }
+
+    private void ChangeFireMode(float LeftB, float RightB)
+    {
+        if (LeftB == 1 && RightB == 1)
+            return;
+        else if (LeftB == 1)
+        {
+            if (FireMode == 0)
+                FireMode = 2;
+            else
+                FireMode--;
+        }
+        else if (RightB == 1)
+        {
+            if (FireMode == 2)
+                FireMode = 0;
+            else
+                FireMode++;
         }
     }
 }
