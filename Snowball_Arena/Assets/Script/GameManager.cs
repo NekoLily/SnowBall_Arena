7a9f8b2205b5016be893f554cc0f97ee8557963c
Neko <bernard.sourichanh@gmail.com>
2019-06-13 14:53:31 +0200

    Add : Number Player change

--- Snowball_Arena/Assets/Script/GameManager.cs
+++ Snowball_Arena/Assets/Script/GameManager.cs
@@ -1,43 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
-{   
-    [SerializeField]private float mapHeight,spriteHeight,mapWidth,spriteWidth, secBeforeShrink;
-    [SerializeField]private Transform mapParent;
-    [SerializeField]private GameObject snowPrefab;
-    [SerializeField]private GameObject playerPrefab;
-    [SerializeField]private Vector2 spawnPlayerPos1;
-    [SerializeField]private Vector2 spawnPlayerPos2;
-    [SerializeField]private Vector2 spawnPlayerPos3;
-    [SerializeField]private Vector2 spawnPlayerPos4;
-    private int playerNumber;
+{
+    [SerializeField] private float mapHeight, spriteHeight, mapWidth, spriteWidth, secBeforeShrink;
+    [SerializeField] private Transform mapParent;
+    [SerializeField] private GameObject snowPrefab;
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private Vector2 spawnPlayerPos1;
+    [SerializeField] private Vector2 spawnPlayerPos2;
+    [SerializeField] private Vector2 spawnPlayerPos3;
+    [SerializeField] private Vector2 spawnPlayerPos4;
+    private int playerNumber = 2;
+
+    public void IncreasePlayerNumber()
+    {
+        if (playerNumber < 4)
+        {
+            playerNumber++;
+            GameObject.Find("NumberPlayerText").GetComponent<Text>().text = playerNumber.ToString();
+        }
+    }
+
+    public void DecreasePlayerNumber()
+    {
+        if (playerNumber > 2)
+        {
+            playerNumber--;
+            GameObject.Find("NumberPlayerText").GetComponent<Text>().text = playerNumber.ToString();
+        }
+    }
 
     private static GameManager _instance;
-    public static GameManager Instance{
-        get{
-			if (_instance == null){
-				_instance = GameObject.FindObjectOfType<GameManager>();						
-			}		
-			return _instance;
-		}
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
     }
-    private void Awake(){
+    private void Awake()
+    {
         DontDestroyOnLoad(this.gameObject);
     }
     /// <summary>
     /// Instantiate the snow prefab based on the map size
     /// </summary>
-    private void AddSnowToMap(){
-        for(int currentHeightNum = (int)-(mapHeight/spriteHeight)-1; currentHeightNum< (mapHeight/spriteHeight); currentHeightNum++ ){
-            for(int currentWidthNum = -(int)(mapWidth/spriteWidth)/2; currentWidthNum< (mapWidth/spriteWidth)/2; currentWidthNum++){  
-                Instantiate(snowPrefab,new Vector2((currentWidthNum * spriteWidth)+(spriteWidth/2), (currentHeightNum * spriteHeight)+(spriteWidth/2)), Quaternion.identity,mapParent);
+    private void AddSnowToMap()
+    {
+        for (int currentHeightNum = (int)-(mapHeight / spriteHeight) - 1; currentHeightNum < (mapHeight / spriteHeight); currentHeightNum++)
+        {
+            for (int currentWidthNum = -(int)(mapWidth / spriteWidth) / 2; currentWidthNum < (mapWidth / spriteWidth) / 2; currentWidthNum++)
+            {
+                Instantiate(snowPrefab, new Vector2((currentWidthNum * spriteWidth) + (spriteWidth / 2), (currentHeightNum * spriteHeight) + (spriteWidth / 2)), Quaternion.identity, mapParent);
             }
         }
     }
 
-    IEnumerator WaitBeforeShrinking(){
+    IEnumerator WaitBeforeShrinking()
+    {
         yield return new WaitForSeconds(secBeforeShrink);
         LimitScript.Instance.canShrink = true;
     }
@@ -45,7 +72,8 @@ public class GameManager : MonoBehaviour
     private void SetupGame()
     {
         AddSnowToMap();
-        for(int i = 0; i<playerNumber;i++){
+        for (int i = 0; i < playerNumber; i++)
+        {
             GameObject tmpGameObject = Instantiate(playerPrefab, spawnPlayerPos1, playerPrefab.transform.rotation);
             tmpGameObject.GetComponent<PlayerScript>().ChangeKeyCode(KeyCodeSave.Instance.GiveOneDimension(i));
         }
