ff34f0c45f0e650e825f1545c550644bf33e34d8
François Charrier <francois.charrier23@gmail.com>
2019-06-13 09:47:02 +0200

    Création d'un script pour la limite de la map

---
+++ Snowball_Arena/Assets/LimitScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitScript : MonoBehaviour
+{   
+    private Transform limitTransform;
+    public bool canShrink{get;set;}=false;
+    private static LimitScript _instance;
+    public static LimitScript Instance{
+        get{
+			if (_instance == null){
+				_instance = GameObject.FindObjectOfType<LimitScript>();						
+			}		
+			return _instance;
+		}
+    }
+
+    private void Awake() {
+        limitTransform = this.gameObject.GetComponent<Transform>();
+    }
+    private void FixedUpdate() {
+        if(canShrink){
+            limitTransform.localScale -= new Vector3(0.001f,0.001f,0);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "PlayerBaseScale"){
+            Debug.Log("Lose");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.tag == "Snow"){
+            Destroy(other.gameObject);
+            //Change sprite instead?
+        }       
+    }
+}
