9d7d78f78a6f5117177015f10bdec4980d0710a3
Neko <bernard.sourichanh@gmail.com>
2019-06-14 14:57:43 +0200

    Add color winner

--- Snowball_Arena/Assets/WinnerScript.cs
+++ Snowball_Arena/Assets/WinnerScript.cs
@@ -1,12 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinnerScript : MonoBehaviour
 {
-    [SerializeField] private Sprite[] ColorPlayers;
+    [SerializeField] private GameObject[] colorPlayers;
+    [SerializeField] private Color[] playerColorArray = { Color.blue, Color.green, Color.yellow, Color.magenta };
     public void Awake()
     {
         
+        string TMP = GameManager.Instance.winnerName;
+        GameObject.Find("WinnerText").GetComponent<Text>().text = TMP + " Win !";
+        
+        switch (TMP)
+        {
+            case "Player 1":
+                GameObject.Find("boule").GetComponent<SpriteRenderer>().color = playerColorArray[0];
+                break;
+            case "Player 2":
+                GameObject.Find("boule").GetComponent<SpriteRenderer>().color = playerColorArray[1];
+                break;
+            case "Player 3":
+                GameObject.Find("boule").GetComponent<SpriteRenderer>().color = playerColorArray[2];
+                break;
+            case "Player 4":
+                GameObject.Find("boule").GetComponent<SpriteRenderer>().color = playerColorArray[3];
+                break;
+        }
     }
 }
