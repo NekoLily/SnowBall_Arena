d4ad072489dff9beace24786d48ad1adbc5fb647
DESKTOP-VEHLP86\squas <lq.millac@hotmail.com>
2019-06-14 14:48:40 +0200

    add : Color

---
+++ Snowball_Arena/Assets/WinnerScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinnerScript : MonoBehaviour
+{
+    [SerializeField] private Sprite[] ColorPlayers;
+    public void Awake()
+    {
+        
+    }
+}
